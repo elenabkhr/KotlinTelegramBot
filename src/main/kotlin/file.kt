48033825436941e0a1dcfd330dbec4f764c233af
Elena Bakherkina <lll.00088@mail.ru>
2025-09-01 19:23:03 +0300

    KTB-05 - add main menu: refactor

--- src/main/kotlin/file.kt
+++ src/main/kotlin/file.kt
@@ -12,32 +12,24 @@ fun loadDictionary(): List<Word> {
     val wordsFile = File("words.txt")
     val dictionary = mutableListOf<Word>()
 
-    if (wordsFile.exists()) {
-        val lines = wordsFile.readLines()
-        for (line in lines) {
-            val line = line.split("|")
-            if (line.size < 3) {
-                println("Строка некорректная")
-                continue
-            } else {
-                dictionary.add(
-                    Word(
-                        original = line[0],
-                        translate = line[1],
-                        correctAnswersCount = line[2].toIntOrNull() ?: 0
-                    )
+    val lines = wordsFile.readLines()
+    for (line in lines) {
+        val parts = line.split("|")
+        if (parts.size < 3) {
+            println("Строка некорректная")
+            continue
+        } else {
+            dictionary.add(
+                Word(
+                    original = parts[0],
+                    translate = parts[1],
+                    correctAnswersCount = parts[2].toIntOrNull() ?: 0
                 )
-            }
+            )
         }
-        for (i in dictionary) {
-            println("${i.original}, ${i.translate}, ${i.correctAnswersCount}")
-        }
-    } else {
-        println("Файл не найден")
     }
     return dictionary
 }
-
 
 fun main() {
     val dictionary = loadDictionary()
